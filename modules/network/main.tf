9a681ea81ab8d6a24d1feb70120c395de50fcd42
Ohad Aiznkot <ohad1254@gmail.com>
2025-11-30 06:47:41 -0800

    final

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -69,7 +69,7 @@ resource "aws_route" "private_nat" {
 }
 
 resource "aws_route_table_association" "private" {
-  count          = 2
+  count          = length(aws_subnet.private)
   subnet_id      = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private.id
 }
